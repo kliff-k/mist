8084fe5c13d4cd550ec7ecb982a66fdaea5fb363
Murilo Moraes <kliffkast@gmail.com>
2021-10-23 10:54:32 -0300

    Update register.php

--- services/register.php
+++ services/register.php
@@ -4,4 +4,4 @@ include "../backend/controller.php";
 
 registerUser($_POST['username'], $_POST['nickname'], $_POST['password'], $_POST['email']);
 
-header('Location: ../admin.php');
+header('Location: ../index.php');
